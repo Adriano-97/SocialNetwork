c6d053655910aac3dc1332fb953243cf0a8ffaf2
Bitlord97 <adriano199726@gmail.com>
2018-12-10 08:15:04 -0500

    I modified the class to include the DB.class. Also added some fields to handle the creation of a new user

--- create-account.php
+++ create-account.php
@@ -1,5 +1,24 @@
 <?php
+include "classes/DB.php";
 $pdo = new PDO('mysql:host=localhost;dbname=socialnetwork;charset=utf8', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "I'm working";
+
+if (isset($_POST['createaccount'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array('username' =>$username, 'password' => $password, 'email' =>$email));
+    echo "Succes";
+}
+
  ?>
+
+
+<h1>Register</h1>
+<form action = "create-account.php" method = "post">
+<input type = "text" name = "username" value = "" placeholder="UserName....."><p />
+<input type = "password" name="password" value="" placeholder="Password....."><p />
+<input type ="email" name="email" value="" placeholder="someone@site.com"><p />
+<input type = "submit" name = "createaccount" value = "Create Account"><p />
+</form>
