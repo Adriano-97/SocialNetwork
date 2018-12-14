6e86fef19977275017ef5f57b997cb224fc3a40a
Bitlord97 <adriano199726@gmail.com>
2018-12-14 15:51:18 -0500

    I added this utility class to avoid writing repetetive code. This code was origianly implemented in login.php.
    #Note: on line 6 im supresing a warning

---
+++ classes/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+class Login {
+
+  public static function isLoggedIn() {
+      error_reporting (E_ALL ^ E_NOTICE);
+
+      if(DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token' => sha1($_COOKIE['SPID'] )))){
+        $user_id = DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token' => sha1($_COOKIE['SPID'])))[0]['user_id'];
+
+        if(isset($_COOKIE['SPID_'])){
+
+          return $user_id;
+        } else {
+          $cstrong =True;
+          $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+          DB::query('DELETE FROM login_tokens WHERE token = :token', array(':token' => sha1($_COOKIE['SPID'])));
+          DB::query('INSERT INTO login_tokens VALUES (NULL, :user_id, :token)', array(':user_id'=> $user_id, ':token'=> sha1($token)));
+
+          setcookie("SPID", $token, time() + $one_week, '/', NULL, NULL, TRUE );
+          setcookie("SPID_", '1', time() + $three_days, '/', NULL, NULL, TRUE );
+
+          return $user_id;
+        }
+      }
+
+      return false;
+    }
+}
+?>
