71563635311df08d0c8df016a78b1048f642e5d0
Bitlord97 <adriano199726@gmail.com>
2018-12-16 13:14:35 -0500

    I moifided the classes so that the users can reset their password #not done

--- change-password.php
+++ change-password.php
@@ -2,12 +2,12 @@
 include ("classes/DB.php");
 include ("classes/Login.php");
 
+$tokenIsValid = False;
 if (Login::isLoggedIn()) {
     if(isset($_POST['changepassword'])){
 
       $user_id = Login::isLoggedIn();
       $oldpassword = $_POST['oldPassword'];
-      $passwordDB = DB::query('SELECT password FROM users WHERE idusers = :userid', array(':userid' => $user_id))[0]['password'];
       $newPassword = $_POST['newPassword'];
       $reNewPassword = $_POST['reNewPassword'];
 
@@ -31,12 +31,44 @@ if (Login::isLoggedIn()) {
       }
     }
 }  else {
+    if(isset($_GET['token'])) {
+      $token = $_GET['token'];
+        if(DB::query('SELECT user_id FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)))) {
+        $user_id = DB::query('SELECT user_id FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)))[0]['user_id'];
+        $tokenIsValid = True;
+
+        if(isset($_POST['changepassword'])){
+              $newPassword = $_POST['newPassword'];
+              $reNewPassword = $_POST['reNewPassword'];
+              echo $newPassword;
+              echo $reNewPassword;
+              if(strlen($newPassword) >= 6 && strlen($newPassword) <= 60) {
+
+                    if($newPassword == $reNewPassword) {
+                        DB::query('UPDATE users SET password =:newPassword WHERE idusers = :userid', array(':newPassword' => password_hash($newPassword, PASSWORD_BCRYPT), ':userid' => $user_id));
+                        echo "Password changed Successfully";
+                    } else {
+                        echo "The new passwords don't match";
+                    }
+
+              } else {
+                    echo "The new password entered is not valid";
+              }
+        }
+      } else {
+        die("Invalid token");
+
+      }
+
+
+  } else {
     die("Not logged in");
+  }
 }
 ?>
 <h1> Change Password </h1>
-<form action="change-password.php" method="post">
-        <input type="password" name="oldPassword" value="" placeholder="Current Password"><p />
+<form action=<?php if(!$tokenIsValid) {echo"change-password.php";} else {echo"change-password.php?token='.$token.'";}?> method="post">
+        <?php if(!$tokenIsValid ){echo'<input type="password" name="oldPassword" value="" placeholder="Current Password"><p />';}?>
         <input type="password" name="newPassword" value="" placeholder="New Password"><p />
         <input type="password" name="reNewPassword" value="" placeholder="Repeat New Password"><p />
         <input type="submit"  name="changepassword" value="Change Password">
---
+++ forgot-password.php
@@ -0,0 +1,28 @@
+<?php
+include('./classes/DB.php');
+error_reporting (E_ALL ^ E_NOTICE);
+
+if  (isset($_POST['submit'])) {
+
+    $cstrong = True;
+    $email= $_POST['email'];
+    $token = bin2hex(openssl_random_pseudo_bytes(42, $cstrong));
+
+    if(DB::query('SELECT idusers FROM users WHERE email = :email', array(':email' => $email))[0]['idusers']){
+      $user_id = DB::query('SELECT idusers FROM users WHERE email = :email', array(':email' => $email))[0]['idusers'];
+      DB::query('INSERT INTO password_tokens VALUES(NULL, :token, :user_id)', array('token'=> sha1($token), ':user_id' => $user_id));
+      echo "Email sent";
+      echo '</br>';
+      echo $token;
+      echo '</br>';
+      echo sha1($token);
+    } else {
+        echo "Not a valid email";
+    }
+}
+ ?>
+ <h1> Forgot Password </h1>
+ <form action="forgot-password.php" method="post">
+   <input type="text" name="email" value="" placeholder="Email...."><p />
+   <input type="submit" name="submit" placeholder="Submit">
+ </form>
