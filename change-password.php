282e4ee360247932c6bec9cc19c2ff984c86e212
Bitlord97 <adriano199726@gmail.com>
2018-12-17 09:37:05 -0500

    I fixed the errors in the form. Now this will change the password if a token is provided.

--- change-password.php
+++ change-password.php
@@ -33,19 +33,20 @@ if (Login::isLoggedIn()) {
 }  else {
     if(isset($_GET['token'])) {
       $token = $_GET['token'];
-        if(DB::query('SELECT user_id FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)))) {
-        $user_id = DB::query('SELECT user_id FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)))[0]['user_id'];
+      $idQuery = DB::query('SELECT user_id FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)));
+        if($idQuery != False) {
+        $user_id = $idQuery[0]['user_id'];
         $tokenIsValid = True;
 
         if(isset($_POST['changepassword'])){
               $newPassword = $_POST['newPassword'];
               $reNewPassword = $_POST['reNewPassword'];
-              echo $newPassword;
-              echo $reNewPassword;
+
               if(strlen($newPassword) >= 6 && strlen($newPassword) <= 60) {
 
                     if($newPassword == $reNewPassword) {
                         DB::query('UPDATE users SET password =:newPassword WHERE idusers = :userid', array(':newPassword' => password_hash($newPassword, PASSWORD_BCRYPT), ':userid' => $user_id));
+                        DB::query('DELETE FROM password_tokens WHERE token = :token', array(':token'=> sha1($token)));
                         echo "Password changed Successfully";
                     } else {
                         echo "The new passwords don't match";
@@ -67,7 +68,7 @@ if (Login::isLoggedIn()) {
 }
 ?>
 <h1> Change Password </h1>
-<form action=<?php if(!$tokenIsValid) {echo"change-password.php";} else {echo"change-password.php?token='.$token.'";}?> method="post">
+<form action=<?php if(!$tokenIsValid) {echo"change-password.php";} else {echo"change-password.php?token={$token}";}?> method="post">
         <?php if(!$tokenIsValid ){echo'<input type="password" name="oldPassword" value="" placeholder="Current Password"><p />';}?>
         <input type="password" name="newPassword" value="" placeholder="New Password"><p />
         <input type="password" name="reNewPassword" value="" placeholder="Repeat New Password"><p />
