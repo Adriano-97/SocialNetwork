cbbf9a16e214f39db334b306971ecc9179ee7685
Bitlord97 <adriano199726@gmail.com>
2018-12-19 20:19:24 -0500

    Users can now follow and unfollow each other. I updated the requirments so that users can't follow themself or follow someone two times.

--- profile.php
+++ profile.php
@@ -4,37 +4,69 @@ include ("classes/Login.php");
 
 $userProfile = '';
 if(isset($_GET['userprofile'])){
+
   $userProfile = $_GET['userprofile'];
+  $isFollowing = False;
+  $follower_id = Login::isLoggedIn();
   $idQuery = DB::query('SELECT idusers FROM users WHERE username = :username ', array(':username'=> $userProfile));
+  $user_id = $idQuery[0]['idusers'];
+  $selectQuery = DB::query('SELECT follower_id FROM followers WHERE user_id = :user_id', array(':user_id' => $user_id));
+
   if($idQuery) {
 
-    if(isset($_POST['follow'])) {
-      if(Login::isLoggedIn() != False){
-        $user_id = $idQuery[0]['idusers'];
-        $follower_id = Login::isLoggedIn();
+    if($selectQuery) {
+      $isFollowing = True;
+    }
+    if (!($user_id == $follower_id)) {
+      if(isset($_POST['follow'])) {
+        if(Login::isLoggedIn() != False){
 
-        if(!DB::query('SELECT follower_id FROM followers WHERE user_id = :user_id', array(':user_id' => $user_id))) {
-          DB::query('INSERT INTO followers VALUES (NULL, :userid, :follower_id)', array(':userid'=> $user_id, ':follower_id' => $follower_id));
+          if(!$selectQuery) {
+            DB::query('INSERT INTO followers VALUES (NULL, :userid, :follower_id)', array(':userid'=> $user_id, ':follower_id' => $follower_id));
+          } else {
+            echo "Already following";
+          }
         } else {
-          echo "Already following";
+          echo "You must be loging to follow an user.";
         }
+        $isFollowing = True;
       } else {
-        echo "You must be loging to follow an user.";
+        //do nothing
       }
     } else {
-      //do nothing
+      echo "Your profile";
+    }
+
+    if (!($user_id == $follower_id)) {
+      if(isset($_POST['unfollow'])) {
+
+        if($selectQuery) {
+          DB::query('DELETE FROM followers WHERE user_id=:user_id AND follower_id=:follower_id', array(':user_id'=> $user_id, ':follower_id' => $follower_id));
+          $isFollowing = False;
+        } else {
+          echo "Not Following";
+        }
+      }
     }
   } else {
     die('User not found');
   }
 
 } else {
-
   die('User not found');
 }
+
 
 ?>
 <h1> <?php echo $userProfile; ?>'s Profile<h1>
   <form action="profile.php?userprofile=<?php echo $userProfile; ?>" method="post">
-    <input  type="submit" name="follow" value="Follow">
+    <?php
+      if (!($user_id == $follower_id)) {
+        if (!$isFollowing) {
+          echo '<input  type="submit" name="follow" value="Follow"><p />';
+        } else {
+          echo '<input type ="submit" name= "unfollow" value = "Unfollow"><p />';
+        }
+      }
+    ?>
   </form>
