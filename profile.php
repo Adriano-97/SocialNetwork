db30bbc4dbcd697d5627fb343760e6ad319d9aad
Bitlord97 <adriano199726@gmail.com>
2018-12-21 15:53:10 -0500

    I modified the profile class so that the user can like and unlike posts

--- profile.php
+++ profile.php
@@ -10,7 +10,8 @@ if(isset($_GET['userprofile'])){
   $follower_id = Login::isLoggedIn();
   $idQuery = DB::query('SELECT idusers FROM users WHERE username = :username ', array(':username'=> $userProfile));
   $user_id = $idQuery[0]['idusers'];
-  $selectQuery = DB::query('SELECT follower_id FROM followers WHERE user_id = :user_id', array(':user_id' => $user_id));
+  $selectQuery = DB::query('SELECT follower_id FROM followers WHERE user_id = :user_id AND follower_id=:follower_id', array(':user_id'=> $user_id, ':follower_id' => $follower_id));
+  //$likeQuery = DB::query('SELECT post_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$_GET['postid'],':user_id' =>$follower_id ));
 
   if($idQuery) {
 
@@ -51,18 +52,47 @@ if(isset($_GET['userprofile'])){
 
     if(isset($_POST['post'])) {
         $postbody = $_POST['postbody'];
-        $userid = Login::isLoggedIn();
+
 
         if(strlen($postbody) > 160 || strlen($postbody) < 1) {
           die('Incorrect Lenght');
         }
-        DB::query('INSERT INTO  posts VALUES(NULL, :postbody, NOW(), :user_id, \'0\')', array(':postbody'=> $postbody, ':user_id' => $user_id));
+        DB::query('INSERT INTO  posts VALUES(NULL, :postbody, NOW(), :user_id, \'0\')', array(':postbody'=> $postbody, ':user_id' => $follower_id));
 
+    }
+
+    if(isset($_GET['postid'])){
+      if(!DB::query('SELECT user_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$_GET['postid'],':user_id' =>$follower_id ))) {
+
+        DB::query('UPDATE posts SET likes = likes + 1 WHERE id =:postid', array (':postid'=> $_GET['postid']));
+        DB::query('INSERT INTO post_likes VALUES(NULL,:post_id, :user_id )', array(':post_id' =>$_GET['postid'], ':user_id' =>$follower_id ));
+
+      } else {
+
+        DB::query('UPDATE posts SET likes = likes - 1 WHERE id =:postid', array (':postid'=> $_GET['postid']));
+        DB::query('DELETE FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$_GET['postid'], ':user_id' =>$follower_id ));
+
+      }
     }
 
     $dbposts = DB::query('SELECT * FROM posts WHERE user_id = :userid ORDER BY id DESC',array(':userid' => $user_id));
     foreach ($dbposts as $p) {
-       $posts .= $p['body']."</br /><hr>";
+
+       if(!DB::query('SELECT post_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$p['id'],':user_id' =>$follower_id ))) {
+         $posts .= htmlspecialchars($p['body'])."
+                    <form action= 'profile.php?userprofile=$userProfile&postid=".$p['id']."' method='post'>
+                          <input type= 'submit' name= 'like' value= 'Like'>
+                          <span>".$p['likes']." likes </span>
+                      </form>
+                    </br /><hr>";
+      } else {
+        $posts .= htmlspecialchars($p['body'])."
+                   <form action= 'profile.php?userprofile=$userProfile&postid=".$p['id']."' method='post'>
+                         <input type= 'submit' name= 'unlike' value= 'Unlike'>
+                         <span>".$p['likes']." likes </span>
+                     </form>
+                   </br /><hr>";
+      }
     }
 
   } else {
@@ -87,11 +117,13 @@ if(isset($_GET['userprofile'])){
       }
     ?>
   </form>
-
-  <form action="profile.php?userprofile=<?php echo $userProfile; ?>" method="post">
-    <textarea name="postbody" rows="8" cols="80"> </textarea>
-    <input type="submit" name="post" value = "Post" >
-  </form>
+  <?php if($user_id == $follower_id ) {
+    echo '<form action="profile.php?userprofile=';echo $userProfile; echo' " method="post">
+      <textarea name="postbody" rows="8" cols="80"> </textarea>
+      <input type="submit" name="post" value = "Post" >
+    </form>';
+  }
+    ?>
 
   <div class="posts">
       <?php  echo $posts;?>
