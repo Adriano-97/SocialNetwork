427b29908e10c88fd802ef24bfc5f44a1ba03ca8
Bitlord97 <adriano199726@gmail.com>
2018-12-26 17:11:57 -0500

    The index page will now show the posts of the users that are being followed, it will also permit the user too like/unlike posts.

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 include ("classes/DB.php");
 include ("classes/Login.php");
+include ("classes/Post.php");
 $showTimeline = False;
 if (Login::isLoggedIn()) {
     echo "Logged in <hr/>";
     $user_id =  Login::isLoggedIn();
     $showTimeline = True;
 } else {
-    echo "Not logged in";
+    die ("Not logged in");
 }
 
 if($showTimeline != False){
-  $followingposts = DB::query('SELECT posts.body, posts.posted_at, posts.likes, users.username
+
+  $followingposts = DB::query('SELECT posts.body, posts.posted_at, posts.likes, users.username, posts.id
     FROM followers, posts, users
     WHERE  posts.user_id = followers.user_id
     AND users.idusers = posts.user_id
@@ -19,7 +21,24 @@ if($showTimeline != False){
     ORDER BY posts.posted_at DESC', array (':user_id' => $user_id));
 
     foreach ($followingposts as $post) {
-      echo $post['body']." ~ ".$post['username']."<hr/>";
+      if(!DB::query('SELECT post_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$post['id'],':user_id' =>$user_id ))) {
+                   echo $post['body']." ~ ".$post['username'];
+                   echo "<form action= 'index.php?postid=".$post['id']."' method='post'>
+                         <input type= 'submit' name= 'like' value= 'Like'>
+                         <span>".$post['likes']." likes </span>
+                     </form>
+                   </br /><hr>";
+     } else {
+                  echo $post['body']." ~ ".$post['username'];
+                  echo "<form action= 'index.php?postid=".$post['id']."' method='post'>
+                        <input type= 'submit' name= 'unlike' value= 'Unlike'>
+                        <span>".$post['likes']." likes </span>
+                    </form>
+                  </br /><hr>";
+     }
+
+
     }
+    Post::likingPost($_GET['postid'], $user_id);
 }
 ?>
