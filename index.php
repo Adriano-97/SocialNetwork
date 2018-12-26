a4a74d02e15d60cd04c0ae614345392b00695786
Bitlord97 <adriano199726@gmail.com>
2018-12-26 18:40:28 -0500

    Now users can add comments to the posts. #Comments aren't dysplayed yet.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include ("classes/DB.php");
 include ("classes/Login.php");
 include ("classes/Post.php");
+include ("classes/Comments.php");
 $showTimeline = False;
 if (Login::isLoggedIn()) {
     echo "Logged in <hr/>";
@@ -11,6 +12,13 @@ if (Login::isLoggedIn()) {
     die ("Not logged in");
 }
 
+if(isset($_GET['postid'])) {
+  Post::likingPost($_GET['postid'], $user_id);
+}
+
+if(isset($_POST['commentBody'])) {
+  Comments::createComment($_POST['commentBody'], $user_id, $_GET['postidtome']);
+}
 if($showTimeline != False){
 
   $followingposts = DB::query('SELECT posts.body, posts.posted_at, posts.likes, users.username, posts.id
@@ -24,21 +32,24 @@ if($showTimeline != False){
       if(!DB::query('SELECT post_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$post['id'],':user_id' =>$user_id ))) {
                    echo $post['body']." ~ ".$post['username'];
                    echo "<form action= 'index.php?postid=".$post['id']."' method='post'>
-                         <input type= 'submit' name= 'like' value= 'Like'>
-                         <span>".$post['likes']." likes </span>
-                     </form>
-                   </br /><hr>";
+                          <input type= 'submit' name= 'like' value= 'Like'>
+                          <span>".$post['likes']." likes </span>
+                         </form>";
      } else {
                   echo $post['body']." ~ ".$post['username'];
                   echo "<form action= 'index.php?postid=".$post['id']."' method='post'>
                         <input type= 'submit' name= 'unlike' value= 'Unlike'>
                         <span>".$post['likes']." likes </span>
-                    </form>
-                  </br /><hr>";
-     }
+                    </form>";
 
+     }
+      echo   "<form action= 'index.php?postidtome={$post['id']}' method='post'>
+                <textarea name='commentBody' rows='6' cols='40'> </textarea>
+                <input type='submit' name='comment' value = 'Post comment' >
+              </form>
+            </br /><hr>";
 
     }
-    Post::likingPost($_GET['postid'], $user_id);
+
 }
 ?>
