d620cf3b0b69afb6ff96b903585a59b0a95f56f1
Bitlord97 <adriano199726@gmail.com>
2018-12-26 22:10:33 -0500

    The comments will now be dysplayed under the posts.

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if($showTimeline != False){
     WHERE  posts.user_id = followers.user_id
     AND users.idusers = posts.user_id
     AND  followers.follower_id = :user_id
-    ORDER BY posts.posted_at DESC', array (':user_id' => $user_id));
+    ORDER BY posts.id DESC', array (':user_id' => $user_id));
 
     foreach ($followingposts as $post) {
       if(!DB::query('SELECT post_id FROM post_likes WHERE post_id =:post_id AND user_id = :user_id ', array(':post_id' =>$post['id'],':user_id' =>$user_id ))) {
@@ -46,8 +46,9 @@ if($showTimeline != False){
       echo   "<form action= 'index.php?postidtome={$post['id']}' method='post'>
                 <textarea name='commentBody' rows='6' cols='40'> </textarea>
                 <input type='submit' name='comment' value = 'Post comment' >
-              </form>
-            </br /><hr>";
+              </form>";
+              Comments::showComment($post['id']);
+      echo    "</br /><hr>";
 
     }
 
