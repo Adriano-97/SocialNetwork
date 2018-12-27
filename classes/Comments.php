4d1c266c45691454520254ae692b91716a957e2d
Bitlord97 <adriano199726@gmail.com>
2018-12-26 22:11:25 -0500

    I added the utility function showComment.

--- classes/Comments.php
+++ classes/Comments.php
@@ -8,5 +8,16 @@ class Comments {
     DB::query('INSERT INTO  comments VALUES(NULL, :commentBody, :user_id, NOW(), :post_id)', array(':commentBody'=> $commentBody, ':user_id' => $user_id, ':post_id' => $post_id));
 
   }
+
+  public static function showComment($postid){
+    $comments = DB::query('SELECT comments.comment, users.username
+                           FROM comments, users
+                           WHERE post_id = :postid AND comments.user_id = users.idusers
+                           ORDER BY comments.id ASC',
+                           array(':postid'=> $postid));
+    foreach($comments as $com) {
+      echo $com['comment']." ~ ".$com['username']."</br>";
+    }
+  }
 }
 ?>
