d9be33df6e39620045b8123af29457aaaf44aef3
Bitlord97 <adriano199726@gmail.com>
2018-12-29 20:42:14 -0500

    This database class will conect the RESTful API with the wepsite #Unlike the DB of the webpage this one dosn't have static methods.

---
+++ API/DB.php
@@ -0,0 +1,23 @@
+<?php
+class DB {
+
+      private $pdo;
+
+      public  function __construct($host, $port, $dbName, $user, $password) {
+
+        $pdo = new PDO("mysql:host={$host};port={$port};dbname={$dbName}", "{$user}", "{$password}");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this -> pdo = $pdo;
+
+      }
+      public  function query($query, $params =array()) {
+        $statament = $this->pdo->prepare($query);
+        $statament->execute($params);
+
+        if (explode(' ', $query)[0] == 'SELECT'){
+          $data = $statament->fetchAll();
+          return $data;
+        }
+      }
+}
+?>
